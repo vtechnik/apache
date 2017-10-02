399cddeeb4a19427b1982a260597867aec84a777
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-10-02 22:08:54 +0200

    Add

---
+++ test.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Bootstrap 99999999 Template <?php echo isset($_REQUEST['tttt'])?$_REQUEST['tttt']: '-';?></title>
+  </head>
+  <body>
+    <h1>Hello, world!@</h1>
+	<a href="test.php?tttt=6666" class="menuIcon1"><span class="userIcon-Home"></span></a>
+	<a href="test.php?tttt=4444" class="menuIcon2"><span class="userIcon-Home"></span></a>
+	<a href="test.php?tttt=222" class="menuIcon3"><span class="userIcon-Home"></span></a>
+  </body>
+</html>
